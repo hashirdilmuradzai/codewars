a920d4cdec93be9c84f3d6a853f5903ffa5f318e
Hashir Dilmurad Zai <dzhashirdilmuradzai12@gmail.com>
2026-07-31 18:22:38 +0500

    Add solution - CodeHub

---
+++ 8-kyu/drink-about/drink-about.js
@@ -0,0 +1,12 @@
+function peopleWithAgeDrink(old) {
+  if (old >= 0 && old <= 13) {
+    return "drink toddy";
+  } else if (old >= 14 && old <= 17) {
+    return "drink coke";
+  } else if (old >= 18 && old <= 20) {
+    return "drink beer";
+  } else if (old >= 21 && old <= 30) {
+    return "drink whisky";
+  }
+}
+​
